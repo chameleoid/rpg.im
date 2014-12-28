48e2da29643ad162b623875551f25d35e2c15a74
rummik <r@9k1.us>
2014-12-28 04:43:06 -0500

    Rework CSRF wrapper to include more than just post

--- assets/js/dependencies/csrf.js
+++ assets/js/dependencies/csrf.js
@@ -1,16 +1,23 @@
 (function(_csrf, io) {
+  var methods = ['post', 'put', 'delete'];
+
   io.socket.on('connect', function() {
     io.socket.get('/csrfToken', function(data) {
-      //_csrf = data._csrf;
+      _csrf = data._csrf;
     });
 
-    if (!io.socket._post) {
-      io.socket._post = io.socket.post;
+    if (!io.socket._request_) {
+      io.socket._request_ = io.socket._request;
 
-      io.socket.post = function(url, data, cb) {
-        data = data || {};
-        data._csrf = _csrf;
-        return io.socket._post(url, data, cb);
+      io.socket._request = function(options, cb) {
+        options = options || {};
+
+        if (methods.indexOf(options.method) > -1) {
+          options.data = options.data || {};
+          options.data._csrf = options.data._csrf || _csrf;
+        }
+
+        return io.socket._request_(options, cb);
       };
     }
   });
